3b4d078a19c186403ef33a729b1c6c3194b1f05d
YamadaShota <yamada_shota@lightcafe.co.jp>
2023-08-13 23:11:17 +0900

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into issues (summary, description) values ('バグA', 'バグがあります');
+insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
+insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
+
+insert into users (username, password, authority) values ('tom', '664146995a4a8c8bab4a9c6eb12286e7bbd5893a689ed66a8075fc795aa31af69e4210ab374178ae', 'ADMIN');
+insert into users (username, password, authority) values ('bob', '737a35453c202aea3673855676ff58d0a2a2c53d01ba374008767fb49b4b32a31256ea817e47e6c6', 'USER');
